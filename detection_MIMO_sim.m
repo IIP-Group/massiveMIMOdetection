5de2e5c19d29e7ffb7c62916b40dfebe33b6eedb
Charles Jeon <charles.g.jeon@gmail.com>
2020-11-25 01:27:41 -0800

    LAMA detector added by Charles Jeon
    * Added LAMA
    
    * Deleted mat files
    
    * Delete detection_MIMO_sim.asv

--- detection_MIMO_sim.m
+++ detection_MIMO_sim.m
@@ -23,22 +23,22 @@ function detection_MIMO_sim(varargin)
         
     % set default simulation parameters     
     par.runId = 0;         % simulation ID (used to reproduce results)
-    par.MR = 32;           % receive antennas 
-    par.MT = 16;           % transmit antennas (set not larger than MR!)         
-    par.mod = 'QPSK';      % modulation type: 'BPSK','QPSK','16QAM','64QAM'             
-    par.trials = 1e4;      % number of Monte-Carlo trials (transmissions)
+    par.MR = 64;           % receive antennas 
+    par.MT = 64;           % transmit antennas (set not larger than MR!)         
+    par.mod = '16QAM';      % modulation type: 'BPSK','QPSK','16QAM','64QAM'             
+    par.trials = 5e2;      % number of Monte-Carlo trials (transmissions)
     par.simName = ...      % simulation name (used for saving results)
       ['ERR_', num2str(par.MR), 'x', num2str(par.MT), '_', ...
         par.mod, '_', num2str(par.trials),'Trials'];
     par.SNRdB_list = ...   % list of SNR [dB] values to be simulated        
-      0:2:12;
+      15:2:25;
     par.los = 0;           % use line-of-sight (LoS) channel model
     par.chest = 'PERF';    % channel estimator to use: Options:
                            % 'PERF', 'ML', 'BEACHES'
     par.detector = ...     % define detector(s) to be simulated. Options:
      {'SIMO','MMSE',...    % 'SIMO', 'ML', 'MRC', 'ZF', 'MMSE', 'SDR',
-      'TASER','ADMIN',...  % 'TASER', 'RBR', 'ADMIN', 'BOX', 'OCD_MMSE',
-      'OCD_BOX','KBEST'};  % 'OCD_BOX', 'KBEST'
+      'ADMIN',...  % 'TASER', 'RBR', 'ADMIN', 'BOX', 'OCD_MMSE',
+      'OCD_BOX','LAMA'};  % 'OCD_BOX', 'KBEST', 'LAMA'
                            % NOTE: 'ML' and 'SDR' take a long time if used
                            %       for large systems 
                            % NOTE: 'SDR' requires CVX, available here:
@@ -87,6 +87,11 @@ function detection_MIMO_sim(varargin)
     
     % K-BEST parameters ---------------------------------------------------
     par.KBEST.K = 5;          % Number of best nodes to consider at a time
+    
+    % LAMA parameters ---------------------------------------------------
+    par.LAMA.iters = 30;       % Number of LAMA iterations
+    par.LAMA.theta_tau_s = 0.5;  % Damping constant for moment variance param, (0,1)
+    par.LAMA.theta_tau_z = 0.5;  % Damping constant for signal variance param, (0,1)
     
   else
       
@@ -218,6 +223,8 @@ function detection_MIMO_sim(varargin)
             [idxhat,bithat] = BOX(par,Hest,y);
           case 'OCD_MMSE'            % OCD MMSE detector
             [idxhat,bithat] = OCD_MMSE(par,Hest,y,N0);
+          case 'LAMA'                % LAMA detector
+            [idxhat,bithat] = LAMA(par,Hest,y,N0);
           case 'OCD_BOX'             % OCD BOX detector
             [idxhat,bithat] = OCD_BOX(par,Hest,y);            
           case 'KBEST'               % K-Best detector
@@ -658,6 +665,163 @@ function [idxhat,bithat] = OCD_MMSE(par,H,y,N0)
   bithat = par.bits(idxhat,:);
 
 end
+
+
+
+%% LAMA (Large MIMO Approximate Message Passing)
+% -- Charles Jeon, Oscar Castañeda, and Christoph Studer
+% -- "A 354 Mb/s 0.37 mm2 151 mW 32-User 256-QAM Near-MAP Soft-Input
+% Soft-Output Massive MU-MIMO Data Detector in 28nm CMOS," 
+% -- IEEE Solid-State Circuits Letters
+% -- vol. 2, no. 9, pp. 127-130, Oct. 2019.
+%
+% For original algorithm, please see:
+% -- Charles Jeon, Ramina Ghods, Arian Maleki, Christoph Studer
+% -- "Optimality of large MIMO detection via approximate message passing,"
+% -- IEEE International Symposium on Information Theory (ISIT)
+% -- pp. 1227-1231, June. 2015.
+
+function [idxhat,bithat] = LAMA(par,H,y,N0)
+
+%% Transform MR x MT system to MT x MT by utilizing Gram/matched filter outputs
+
+% -- compute Gram matrix
+G = H'*H;
+
+% -- normalize Gram
+Gtilde = eye(par.MT) - diag(1./diag(G))*G;
+
+% -- compute column gains
+g = diag(G)/par.MR;
+
+% -- compute inverse col gain
+gtilde = 1./diag(G);
+
+% -- compute matched filter
+yMF = H'*y;
+
+% -- compute normalized matched filter
+yMFtilde = gtilde.*yMF;
+
+%% Detector parameters
+
+% -- non-linear MMSE estimate
+shat   = zeros(par.MT,par.LAMA.iters+1);
+tau_s  = zeros(par.MT,par.LAMA.iters+1);
+tau_p  = zeros(1 ,    par.LAMA.iters+1);
+
+% -- signal mean/var estimate (modeled as s + Gaussian noise)
+z      = zeros(par.MT,par.LAMA.iters+1);
+tau_z = zeros(par.MT,par.LAMA.iters+1);
+
+% -- Onsager term
+v     = zeros(par.MT,par.LAMA.iters+1);
+
+
+%% initialize estimates
+% -- assume input signal has variance par.Es
+tau_s(:,1) = par.Es * ones(par.MT,1);
+tau_p(1) = g' * tau_s(:,1);
+
+% -- first signal estimate is matched filter
+z(:,1) = yMFtilde;
+tau_z(:,1) = (tau_p(1) + N0)*gtilde;
+
+% -- damping parameters
+theta_tau_s = par.LAMA.theta_tau_s;
+theta_tau_z = par.LAMA.theta_tau_z;
+
+%% -- Loop
+for k=1:par.LAMA.iters
+    
+    % -- Compute moments - mean/var
+    [shat(:,k+1), tau_s(:,k+1)] = LAMA_MeanVar(par, z(:,k), tau_z(:,k));
+    
+    % -- damp second moment estimate
+    tau_p(k+1) = theta_tau_s * ( g' * tau_s(:,k+1) ) + (1 - theta_tau_s) * tau_p(k);
+    
+    % -- Onsager term
+    v(:,k+1) = tau_p(k+1) / (tau_p(k) + N0) * ( z(:,k) - shat(:,k) );
+    
+    % -- damp signal variance estimate
+    tau_z(:,k+1) = theta_tau_z * (tau_p(k+1) + N0) * gtilde + (1-theta_tau_z) * tau_z(:,k);
+    
+    % -- signal update
+    z(:,k+1) = yMFtilde + Gtilde * shat(:,k+1) + v(:,k+1);
+    
+end
+
+% -- output signal
+z_out = z(:,end);
+
+% -- compute outputs
+[~,idxhat] = min(abs(z_out*ones(1,length(par.symbols))-ones(par.MT,1)*par.symbols).^2,[],2);
+bithat = par.bits(idxhat,:);
+
+end
+
+function [F,G] = LAMA_MeanVar(par, z, sigma_sq)
+
+switch par.mod
+    % for BPSK in complex-domain
+    case 'BPSK'
+        
+        F = tanh(2*real(z)./(sigma_sq));
+        G =  1 - abs(F).^2;
+        
+        % compute estimate while considering numerical accuracy
+    otherwise
+        
+        % -- compute input matrices
+        inputM = z*ones(1,length(par.symbols));
+        
+        % -- compute symbol matrices
+        symbolM = ones(length(z),1)*par.symbols;
+        sigma_sqM = sigma_sq*ones(1,length(par.symbols));
+        
+        % -- compute distance
+        input_symM = abs(inputM - symbolM).^2;
+        [~, index_vec] = min(input_symM,[],2);
+        
+        symbolMinM = (par.symbols(index_vec).')*(ones(1,length(par.symbols)));
+        expo = -(input_symM - abs(inputM - symbolMinM).^2);
+        
+        % -- compute pdf
+        pdf = exp(expo./(sigma_sqM));
+        pdf(isnan(pdf)) = 1;
+        
+        sumM = sum(pdf,2)*ones(1,length(par.symbols));
+        
+        % -- compute weight constant
+        w = pdf./sumM;
+        
+        % -- compute mean (F)
+        F = w*(par.symbols.');
+        symbolM2 = ones(length(F),1)*par.symbols;
+        FM = F*ones(1,length(par.symbols));
+        
+        % -- compute variance (G)
+        G = sum(w.*(abs(symbolM2-FM).^2),2);
+        
+        if any(isnan(pdf))|any(isinf(pdf))
+            warning('nan or inf OCCURRED');
+        end
+        
+        
+end
+
+end
+
+
+
+
+
+
+
+
+
+
+
 
 %% Optimized Coordinate Descent (OCD) BOX version
 % -- Michael Wu, Chris Dick, Joseph R. Cavallaro, and Christoph Studer,
